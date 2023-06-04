418bc866f0a2ea78a5f987a0a705c13a82efda6c
Gustavo Inacio <incio.gusta@gmail.com>
2023-06-04 18:23:29 -0300

    format files

--- src/IModeratio.sol
+++ src/IModeratio.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.13;
 
 interface IModeratio {
-  // Create case for moderation
-  // returns case id
-  function createCase() external returns (uint256);
+    // Create case for moderation
+    // returns case id
+    function createCase() external returns (uint256);
 
-  function rule(uint256 _caseId, uint256 _decision) external;
-
+    function rule(uint256 _caseId, uint256 _decision) external;
 }
--- src/Moderatio.sol
+++ src/Moderatio.sol
@@ -1,17 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@chainlink/v0.8/functions/dev/0_0_0/FunctionsClient.sol";
+import {Functions, FunctionsClient} from "@chainlink/v0.8/functions/dev/0_0_0/FunctionsClient.sol";
 
 contract Moderatio is FunctionsClient {
+    using Functions for Functions.Request;
 
-  constructor(address oracle) FunctionsClient(oracle) {
+    uint64 subscriptionId = 1;
+    uint32 gasLimit = 1;
 
-  }
+    bytes32 public latestRequestId;
+    bytes public latestResponse;
+    bytes public latestError;
+    uint256 public responseCounter;
 
-  function fulfillRequest(
-      bytes32 requestId,
-      bytes memory response,
-      bytes memory err
-  ) internal virtual override {}
+    constructor(address oracle) FunctionsClient(oracle) {}
+
+    function request(
+        string calldata source,
+        bytes calldata secrets,
+        string[] calldata args
+    ) public returns (bytes32 requestId) {
+        Functions.Request memory req;
+        req.initializeRequest(
+            Functions.Location.Inline,
+            Functions.CodeLanguage.JavaScript,
+            source
+        );
+        if (secrets.length > 0) {
+            req.addRemoteSecrets(secrets);
+        }
+        if (args.length > 0) req.addArgs(args);
+
+        bytes32 assignedReqID = sendRequest(req, subscriptionId, gasLimit);
+        latestRequestId = assignedReqID;
+        return assignedReqID;
+    }
+
+    function fulfillRequest(
+        bytes32,
+        bytes memory response,
+        bytes memory err
+    ) internal virtual override {
+        latestResponse = response;
+        latestError = err;
+        responseCounter = responseCounter + 1;
+    }
 }
